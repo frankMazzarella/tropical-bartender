1842289674c20d9965b71c3031b5949baa3536bd
Frank Mazzarella <mazzarella.frank@gmail.com>
2020-09-05 23:14:17 -0400

    adding uuid as id into drink object

--- src/controllers/OrderDrink.controller.js
+++ src/controllers/OrderDrink.controller.js
@@ -1,7 +1,10 @@
+const uuid = require('uuid');
+
 const DrinkQueueService = require('../services/DrinkQueue.service');
 
 module.exports = (req, res) => {
   const drink = req.body;
+  drink.id = uuid.v4();
   drink.timestamp = new Date().getTime();
   DrinkQueueService.addDrink(drink);
   console.log(`drink ordered: ${JSON.stringify(drink)}`);
