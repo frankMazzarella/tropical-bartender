95a1e973956e3b8d2f89406188d929908e2e6a95
Frank Mazzarella <mazzarella.frank@gmail.com>
2020-09-10 22:10:42 -0400

    addign delete drink order functionality

--- src/services/Socket.service.js
+++ src/services/Socket.service.js
@@ -4,15 +4,18 @@ const DrinkQueueService = require('./DrinkQueue.service');
 
 let io;
 const QUEUE_UPDATE = 'queue update';
+const DELETE_DRINK_ORDER = 'delete drink order';
 
 const init = (httpServer) => {
   io = socketIO(httpServer);
   io.on('connection', (socket) => {
     socket.emit(QUEUE_UPDATE, DrinkQueueService.getQueue());
+    registerEventHandlers(socket);
   });
 }
 
 const removeDrink = (id) => {
+  console.log(`deleting drink order with id: ${id}`);
   DrinkQueueService.removeDrink(id);
   io.emit(QUEUE_UPDATE, DrinkQueueService.getQueue());
 }
@@ -24,6 +27,13 @@ const getQueue = () => {
 const addDrink = (drink) => {
   DrinkQueueService.addDrink(drink);
   io.emit(QUEUE_UPDATE, DrinkQueueService.getQueue());
+}
+
+function registerEventHandlers(socket) {
+  socket.on(DELETE_DRINK_ORDER, (id, callback) => {
+    removeDrink(id);
+    callback();
+  });
 }
 
 module.exports = { init, removeDrink, addDrink, getQueue };
