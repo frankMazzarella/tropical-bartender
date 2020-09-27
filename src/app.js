a0605be01e5e263dbd48a952b97b06d4d1eb3644
Frank Mazzarella <mazzarella.frank@gmail.com>
2020-09-27 10:44:18 -0400

    tweaking startup log and removing todo

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const fs = require('fs');
+const os = require('os');
 
 const drinkList = require('./drinkList.json');
 const SocketService = require('./services/Socket.service');
 
 const port = 3000;
-// TODO: test file system functionality in windows 10
 const packageJson = JSON.parse(fs.readFileSync('package.json').toString());
 const app = express();
 const httpServer = http.createServer(app);
@@ -24,5 +24,5 @@ app.get('/queue', (req, res) => res.render('pages/queue'));
 app.get('/healthcheck', (req, res) => res.json({ uptime: process.uptime(), version: packageJson.version }));
 
 httpServer.listen(port, () => {
-  console.log(`${packageJson.name} v${packageJson.version} is running on port: ${port}`)
+  console.log(`${packageJson.name} v${packageJson.version} has started on port ${port} [host: ${os.hostname}]`);
 });
