fb11e4c2b01896fb41730e5afecdedc21d22afc7
Frank Mazzarella <mazzarella.frank@gmail.com>
2021-05-24 22:17:15 -0400

    separating socket service into namespaces

--- src/services/Socket.service.js
+++ src/services/Socket.service.js
@@ -8,33 +8,43 @@ const QUEUE_UPDATE = 'queue update';
 const DELETE_DRINK_ORDER = 'delete drink order';
 const ADD_DRINK_ORDER = 'add drink order';
 const DRINK_LIST = 'drink list';
+const CONNECTION = 'connection';
+const DRINK_LIST_NAMESPACE = '/drink-list';
+const ORDER_QUEUE_NAMESPACE = '/order-queue';
 
 const init = (httpServer) => {
   io = socketIO(httpServer, { cors: { origin: '*' } });
-  io.on('connection', (socket) => {
-    socket.emit(QUEUE_UPDATE, DrinkService.getDrinkQueue());
+
+  io.of(DRINK_LIST_NAMESPACE).on(CONNECTION, (socket) => {
     socket.emit(DRINK_LIST, DrinkService.getDrinkList());
-    registerEventHandlers(socket);
+  });
+
+  io.of(ORDER_QUEUE_NAMESPACE).on(CONNECTION, (socket) => {
+    socket.emit(QUEUE_UPDATE, DrinkService.getDrinkQueue());
+    handleAddDrinkOrder(socket);
+    handleDeleteDrinkOrder(socket);
   });
 };
 
-function registerEventHandlers(socket) {
-  socket.on(DELETE_DRINK_ORDER, (id, callback) => {
-    console.log(`deleting drink order with id: ${id}`);
-    DrinkService.removeDrinkOrder(id);
-    io.emit(QUEUE_UPDATE, DrinkService.getDrinkQueue());
-    callback();
-  });
-
+const handleAddDrinkOrder = (socket) => {
   socket.on(ADD_DRINK_ORDER, (drink, callback) => {
     const drinkOrder = drink;
     drinkOrder.id = uuid.v4();
     drinkOrder.timestamp = new Date().getTime();
-    console.log(`add drink order: ${JSON.stringify(drinkOrder)}`);
     DrinkService.addDrinkOrder(drinkOrder);
+    console.log(`add drink order: ${JSON.stringify(drinkOrder)}`);
     io.emit(QUEUE_UPDATE, DrinkService.getDrinkQueue());
     callback();
   });
-}
+};
+
+const handleDeleteDrinkOrder = (socket) => {
+  socket.on(DELETE_DRINK_ORDER, (id, callback) => {
+    DrinkService.removeDrinkOrder(id);
+    console.log(`deleting drink order with id: ${id}`);
+    io.emit(QUEUE_UPDATE, DrinkService.getDrinkQueue());
+    callback();
+  });
+};
 
 module.exports = { init };
